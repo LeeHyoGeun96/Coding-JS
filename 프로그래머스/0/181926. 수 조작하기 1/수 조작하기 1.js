12909ece165dffcb113b3fa6549ad6f99a46be91
LeeHyoGeun96 <154794813+LeeHyoGeun96@users.noreply.github.com>
2024-08-06 21:00:11 +0900

    [level 0] Title: 수 조작하기 1, Time: 6.71 ms, Memory: 36.6 MB -BaekjoonHub

--- 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.js
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.js
@@ -1,3 +1,14 @@
+const operations = {
+  w: (n) => n + 1,
+  s: (n) => n - 1,
+  d: (n) => n + 10,
+  a: (n) => n - 10,
+};
+
+function solution(n, control) {
+  return [...control].reduce((prev, op) => operations[op](prev), n);
+}
+/*
 function solution(n, control) {
     let result = n;
     for(let direction of control){
@@ -17,3 +28,4 @@ function solution(n, control) {
     }
     return result;
 }
+*/
