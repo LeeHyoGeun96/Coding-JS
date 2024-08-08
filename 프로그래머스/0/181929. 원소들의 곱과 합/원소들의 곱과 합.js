671574091082547fdcff0766847720ba65263784
LeeHyoGeun96 <154794813+LeeHyoGeun96@users.noreply.github.com>
2024-08-08 10:58:00 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.js
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.js
@@ -1,5 +1,19 @@
 function solution(num_list) {
+    let accMul = 1
+    let accSum = 0
+    for (const num of num_list) {
+        accMul *= num
+        accSum += num
+    }
+    return accMul < accSum ** 2 ? 1 : 0
+}
+
+
+
+/*
+function solution(num_list) {
     const arrMult = num_list.reduce((acc, cur) => acc * cur,1);
     const arrSumSquare = Math.pow(num_list.reduce((acc,cur)=> acc + cur) , 2)
     return arrSumSquare > arrMult ? 1 : 0;
 }
+*/
