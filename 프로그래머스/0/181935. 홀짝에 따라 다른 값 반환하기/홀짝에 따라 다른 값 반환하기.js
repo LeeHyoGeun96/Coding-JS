b9d67e06454ed161d9e05a7c1e6d3d0cd25c2f32
LeeHyoGeun96 <154794813+LeeHyoGeun96@users.noreply.github.com>
2024-08-09 09:32:56 +0900

    [level 0] Title: 홀짝에 따라 다른 값 반환하기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.js
+++ 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.js
@@ -1,4 +1,12 @@
 function solution(n) {
+    if(n%2===1)
+      return  (n+1)/2*((n + 1)/2) ;
+    else
+      return   n*(n+1)*(n+2)/6;
+}
+
+/*
+function solution(n) {
     let arr = Array.from({ length: n }, (_, i) => i + 1);
     return arr.reduce((acc, cur) => {
         if(n % 2 !== 0 && cur <= n && cur % 2 !== 0){
@@ -9,3 +17,4 @@ function solution(n) {
         return acc;
     }, 0);
 }
+*/
