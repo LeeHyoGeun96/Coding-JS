90b9272f7c3eaae5f08b487224a2e8eb98577140
LeeHyoGeun96 <154794813+LeeHyoGeun96@users.noreply.github.com>
2024-08-09 09:38:33 +0900

    [level 0] Title: 두 수의 연산값 비교하기, Time: 0.06 ms, Memory: 33.2 MB -BaekjoonHub

--- 프로그래머스/0/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.js
+++ 프로그래머스/0/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.js
@@ -1,3 +1,9 @@
 function solution(a, b) {
+    return Math.max(Number(`${a}${b}`), a * b * 2)
+}
+
+/*
+function solution(a, b) {
     return +(a + "" + b) > 2 * a * b ? +(a + "" + b) : 2 * a * b;
 }
+*/
