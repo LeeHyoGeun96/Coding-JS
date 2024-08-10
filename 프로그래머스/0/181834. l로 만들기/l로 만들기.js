f0b774c55542b65e823657802277f8f555852e3e
LeeHyoGeun96 <154794813+LeeHyoGeun96@users.noreply.github.com>
2024-08-10 17:50:25 +0900

    [level 0] Title: l로 만들기, Time: 47.59 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/0/181834. l로 만들기/l로 만들기.js
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.js
@@ -1,5 +1,11 @@
 function solution(myString) {
+    return [...myString].map((v) => v < 'l' ? 'l' : v).join('');
+}
+
+/*
+function solution(myString) {
     return [...myString].map((cur) => {
         return cur.charCodeAt(0) <= 'l'.charCodeAt(0) ? 'l' : cur;
     }).join('');
 }
+*/
