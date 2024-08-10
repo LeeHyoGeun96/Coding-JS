617147fe740cd86d8d22d29bbb55f42983a06977
LeeHyoGeun96 <154794813+LeeHyoGeun96@users.noreply.github.com>
2024-08-10 16:08:11 +0900

    [level 0] Title: 접미사 배열, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181909. 접미사 배열/접미사 배열.js
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.js
@@ -1,7 +1,15 @@
 function solution(my_string) {
+  return Array.from(my_string)
+    .map((_, i) => my_string.substring(i))
+    .sort();
+}
+
+/*
+function solution(my_string) {
     const arr = [];
     for(let i = my_string.length - 1; i >= 0 ; i--){
         arr.push(my_string.slice(i));
     }
     return arr.sort();
 }
+*/
