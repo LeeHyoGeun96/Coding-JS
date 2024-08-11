ae0ee9b33c844354b139964e27036f3e302aabbe
LeeHyoGeun96 <154794813+LeeHyoGeun96@users.noreply.github.com>
2024-08-11 22:06:45 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.06 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.js
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.js
@@ -1,3 +1,13 @@
+function solution(arr) {
+    for(let i = 0; i < arr.length; i++) {
+        if (arr[i] === 'l') return arr.slice(0, i);
+        if (arr[i] === 'r') return arr.slice(i + 1);
+    }
+    return [];
+}
+
+
+/*
 function solution(str_list) {
     const lIdx = str_list.indexOf('l');
     const rIdx = str_list.indexOf('r');
@@ -14,3 +24,4 @@ function solution(str_list) {
 
     return [];
 }
+*/
