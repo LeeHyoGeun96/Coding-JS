38dcb820efc280cb5addb8654077d0752b6b7095
LeeHyoGeun96 <154794813+LeeHyoGeun96@users.noreply.github.com>
2024-08-11 21:56:13 +0900

    [level 0] Title: 배열 만들기 6, Time: 82.74 ms, Memory: 63.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.js
@@ -0,0 +1,20 @@
+function solution(arr) {
+    const stk = [];
+    let i = 0;
+    while(i < arr.length){
+        if(stk.length === 0){
+            stk.push(arr[i]);
+            i++;
+        }
+        else if(stk.at(-1) === arr[i]){
+            stk.pop();
+            i++;
+        }
+        else if(stk.at(-1) !== arr[i]){
+            stk.push(arr[i]);
+            i++;
+        }
+    }
+    
+    return stk.length === 0 ? [-1] : stk;
+}
