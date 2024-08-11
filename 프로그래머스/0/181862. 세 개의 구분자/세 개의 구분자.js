2a5a2f7c2acaded94ac1a06616b28a9f348c8e35
LeeHyoGeun96 <154794813+LeeHyoGeun96@users.noreply.github.com>
2024-08-11 16:39:57 +0900

    [level 0] Title: 세 개의 구분자, Time: 32.68 ms, Memory: 61.3 MB -BaekjoonHub

--- 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.js
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.js
@@ -1,5 +1,12 @@
 function solution(myStr) {
+    const list = myStr.split(/[a|b|c]/).filter(str => str);
+    return list.length ? list : ["EMPTY"];
+}
+
+/*
+function solution(myStr) {
     const str = myStr.replace(/[abc]/g, '0');
     const arr = str.split('0').filter((cur) => cur);
     return arr.length === 0 ? ["EMPTY"] : arr;
 }
+*/
