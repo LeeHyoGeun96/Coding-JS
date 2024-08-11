66d3828a4fc8a08aee8427b8d664d94c43a09ab7
LeeHyoGeun96 <154794813+LeeHyoGeun96@users.noreply.github.com>
2024-08-11 17:08:41 +0900

    [level 0] Title: 리스트 자르기, Time: 0.08 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.js
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.js
@@ -1,3 +1,16 @@
+const slices = {
+  1: (num_list, [a, b, c]) => num_list.slice(0, b + 1),
+  2: (num_list, [a, b, c]) => num_list.slice(a),
+  3: (num_list, [a, b, c]) => num_list.slice(a, b + 1),
+  4: (num_list, [a, b, c]) => num_list.slice(a, b + 1).filter((_, i) => i % c === 0),
+};
+
+function solution(n, slicer, num_list) {
+  return slices[n](num_list, slicer);
+}
+
+
+/*
 function solution(n, slicer, num_list) {
     const [a, b, c] = slicer;
     switch(n){
@@ -15,3 +28,4 @@ function solution(n, slicer, num_list) {
             return arr;
     }
 }
+*/
