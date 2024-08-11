93c7ec68eb059fbefaefe0b2f5ea0d2a2355786e
LeeHyoGeun96 <154794813+LeeHyoGeun96@users.noreply.github.com>
2024-08-11 16:09:57 +0900

    [level 0] Title: 수열과 구간 쿼리 1, Time: 4.51 ms, Memory: 35.9 MB -BaekjoonHub

--- 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.js
+++ 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.js
@@ -1,6 +1,15 @@
 function solution(arr, queries) {
+    queries.forEach(([s, e]) => {
+        while (s <= e) arr[s++]++;
+    });
+    return arr;
+}
+
+/*
+function solution(arr, queries) {
     return queries.reduce((acc, cur) =>{
         const [s, e] = cur;
         return acc.map((arrValue, arrIdx) => (arrIdx >= s && arrIdx <= e) ? arrValue + 1 : arrValue)
     } ,arr);
 }
+*/
