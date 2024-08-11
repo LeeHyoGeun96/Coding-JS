af60f8ad58c4a91ae5aea1b20f448f48d093beb7
LeeHyoGeun96 <154794813+LeeHyoGeun96@users.noreply.github.com>
2024-08-11 11:48:59 +0900

    [level 0] Title: 수 조작하기 2, Time: 10.98 ms, Memory: 41.9 MB -BaekjoonHub

--- 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.js
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.js
@@ -1,4 +1,16 @@
 function solution(numLog) {
+    const convert = {
+        '1': 'w', '-1': 's', '10': 'd', '-10': 'a'
+    };
+
+    return numLog.slice(1).map((v, i) => {
+        return convert[v - numLog[i]]
+    }).join('')
+}
+
+
+/*
+function solution(numLog) {
     return numLog.reduce((acc, cur, idx) => {
         const num = numLog[idx + 1] - cur;
         switch(num){
@@ -15,3 +27,4 @@ function solution(numLog) {
         }
     } ,'');
 }
+*/
