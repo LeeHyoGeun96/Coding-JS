b122d96264cb37f66af87d2e44ecebd7d85f4bf3
LeeHyoGeun96 <154794813+LeeHyoGeun96@users.noreply.github.com>
2024-08-11 17:15:42 +0900

    [level 0] Title: qr code, Time: 0.19 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181903. qr code/qr code.js
@@ -0,0 +1,8 @@
+function solution(q, r, code) {
+    return [...code].reduce((acc, cur, idx) => {
+        if(idx % q === r){
+            acc += cur;
+        }
+        return acc;
+        },'');
+}
