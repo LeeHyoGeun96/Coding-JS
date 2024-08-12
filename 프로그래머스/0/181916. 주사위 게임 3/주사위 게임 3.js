3ef070f18c3b271146e4ba1b47fa1734672404c8
LeeHyoGeun96 <154794813+LeeHyoGeun96@users.noreply.github.com>
2024-08-12 22:06:06 +0900

    [level 0] Title: 주사위 게임 3, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.js
+++ 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.js
@@ -1,3 +1,29 @@
+function count(arr) {
+  const counter = new Map();
+  for (const num of arr) {
+    counter.set(num, (counter.get(num) || 0) + 1);
+  }
+  const sortedByCnt = [...counter.keys()].sort((a, b) => counter.get(b) - counter.get(a));
+  const maxCnt = Math.max(...counter.values());
+  return [sortedByCnt, maxCnt];
+}
+
+function solution(a, b, c, d) {
+  const [arr, maxCnt] = count([a, b, c, d]);
+  const [p, q, r, s] = arr;
+  if (arr.length === 1) {
+    return p * 1111;
+  }
+  if (arr.length === 2) {
+    return maxCnt === 2 ? (p + q) * Math.abs(p - q) : (10 * p + q) ** 2;
+  }
+  if (arr.length === 3) {
+    return q * r;
+  }
+  return Math.min(p, q, r, s);
+}
+
+/*
 function solution(a, b, c, d) {
     const scoreMap = new Map();
     for(const num of [a,b,c,d]){
@@ -25,3 +51,4 @@ function solution(a, b, c, d) {
             return Math.min(...scoreMap.keys());
     }
 }
+*/
