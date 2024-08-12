5d3ebbd2f12bc987fe7286940cb27b6c5679d44e
LeeHyoGeun96 <154794813+LeeHyoGeun96@users.noreply.github.com>
2024-08-12 12:24:44 +0900

    [level 0] Title: 배열 조각하기, Time: 2.04 ms, Memory: 36.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.js
@@ -0,0 +1,9 @@
+function solution(arr, query) {
+    return query.reduce((acc, cur, idx) => {
+        if(idx % 2 === 0){
+            return acc.slice(0, cur + 1);
+        } else{
+            return acc.slice(cur);
+        }
+    }, arr);
+}
